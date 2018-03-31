a26ef9a079134aee9311f59bc293735c6ad749fe
Smith Jesko <smithjesko@gmail.com>
2018-03-31 14:10:05 -0500

    Django templating

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from restaurants.views import home
+from restaurants.views import home, about, contact
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home),
+    url(r'^about/$', about),
+    url(r'^contact/$', contact),
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "base.html", {'var': 'tutorial'})
+    return render(request, "home.html", {'var': 'tutorial'})
+
+def about(request):
+    context = {}
+    return render(request, "about.html", context)
+
+def contact(request):
+    context = {}
+    return render(request, "contact.html", context)
